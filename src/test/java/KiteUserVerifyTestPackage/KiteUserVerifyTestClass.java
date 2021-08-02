a1e337130e51379d6aef2ca5237be27b89cf48bc
Admin <Admin@LAPTOP-U8OF8ASM>
2021-08-02 21:01:21 +0530

    [Vijay]: Added Browsers folder, Created Test package and Test Class

---
+++ src/test/java/KiteUserVerifyTestPackage/KiteUserVerifyTestClass.java
@@ -0,0 +1,36 @@
+package KiteUserVerifyTestPackage;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class KiteUserVerifyTestClass {
+	
+	@BeforeClass
+	public void BrowserSetUp() {
+		
+	}
+	
+	@BeforeMethod
+	public void LoginToApplication() {
+		
+	}
+	
+	@Test
+	public void VerifyUserOfApplication() {
+		
+	}
+	
+	@AfterMethod
+	public void LogoutFromApplication() {
+		
+	}
+	
+	@AfterClass
+	public void Close_Set_Up() {
+		
+	}
+
+}
