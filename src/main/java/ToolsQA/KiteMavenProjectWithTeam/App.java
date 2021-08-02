6f568e64c721854fc03bf4fb711e45bb221a5667
Admin <Admin@LAPTOP-U8OF8ASM>
2021-08-02 20:38:22 +0530

    [Vijay]: Created Project.

---
+++ src/main/java/ToolsQA/KiteMavenProjectWithTeam/App.java
@@ -0,0 +1,13 @@
+package ToolsQA.KiteMavenProjectWithTeam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
